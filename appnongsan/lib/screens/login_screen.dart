a3a5e29a77cee0f81e1e402fbefe0217b08f6bf1
hoan <hoan22112003@gmail.com>
2024-08-30 16:11:47 +0700

    set up 2

--- appnongsan/lib/screens/login_screen.dart
+++ appnongsan/lib/screens/login_screen.dart
@@ -1,12 +1,54 @@
+import 'package:appnongsan/reponsive/mobile_screen_layout.dart';
+import 'package:appnongsan/reponsive/reponsive_screen_layout.dart';
+import 'package:appnongsan/reponsive/web_screen_layout.dart';
+import 'package:appnongsan/resources/auth_methods.dart';
+import 'package:appnongsan/screens/forget_password_screen.dart';
+import 'package:appnongsan/screens/sign_up_screen.dart';
+import 'package:appnongsan/utils/utils.dart';
 import 'package:appnongsan/widgets/login_button.dart';
 import 'package:appnongsan/widgets/login_textfield.dart';
+import 'package:appnongsan/widgets/password_field.dart';
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
   Widget build(BuildContext context) {
+    TextEditingController emailController = TextEditingController();
+    TextEditingController passController = TextEditingController();
+    bool isLoading = false;
+
+    void logInUser() async {
+      setState(() {
+        isLoading = true;
+      });
+      String res = await AuthMethods().logInUser(
+          email: emailController.text, password: passController.text);
+      setState(() {
+        isLoading = false;
+      });
+      if (res == 'success') {
+        if (context.mounted) {
+          Navigator.of(context).pushReplacement(
+            MaterialPageRoute(
+              builder: (context) => const ReponsiveLayout(
+                mobileScreenLayout: MobileScreenLayout(),
+                webScreenLayout: WebScreenLayout(),
+              ),
+            ),
+          );
+        }
+      } else {
+        showSnackBar(res, context);
+      }
+    }
+
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
@@ -15,21 +57,52 @@ class LoginScreen extends StatelessWidget {
             const SizedBox(
               height: 32,
             ),
-            LoginTextfield(hintText: 'Số điện thoại hoặc gmail'),
+            LoginTextfield(
+              hintText: 'Tên tài khoản',
+              editingController: emailController,
+            ),
             const SizedBox(
               height: 24,
             ),
-            LoginTextfield(hintText: 'Mật khẩu'),
+            PasswordTextfield(
+              hintText: 'Mật khẩu',
+              editingController: passController,
+            ),
             const SizedBox(
               height: 12,
             ),
-            const Padding(
-                padding: EdgeInsets.only(left: 240),
-                child: Text('Quên mật khẩu ?')),
+            Padding(
+              padding: EdgeInsets.only(left: 240),
+              child: InkWell(
+                onTap: () => Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                        builder: (context) => ForgetPasswordScreen()
+                        )
+                        )
+                        ,
+                child: Text('Quên mật khẩu ?'),
+              ),
+            ),
             const SizedBox(
               height: 12,
             ),
-            const LoginButton(),
+            Container(
+              width: 350,
+              height: 54,
+              child: TextButton(
+                onPressed: logInUser,
+                child: Text(
+                  'Đăng nhập',
+                  style: TextStyle(color: Colors.white),
+                ),
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.green, // Màu nền của nút
+
+                  padding: EdgeInsets.symmetric(
+                      vertical: 12.0, horizontal: 24.0), // Padding của nút
+                ),
+              ),
+            ),
             const SizedBox(
               height: 24,
             ),
@@ -57,10 +130,22 @@ class LoginScreen extends StatelessWidget {
                 ])
               ],
             ),
-            SizedBox(height: 30,),
+            SizedBox(
+              height: 30,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: [Text('Chưa có tài khoản? '), Text('Đăng ký', style: TextStyle(color: Colors.green),)],
+              children: [
+                Text('Chưa có tài khoản? '),
+                InkWell(
+                  onTap: () => Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) => SignUpScreen())),
+                  child: Text(
+                    'Đăng ký',
+                    style: TextStyle(color: Colors.green),
+                  ),
+                )
+              ],
             )
           ],
         ),
--- appnongsan/lib/widgets/login_textfield.dart
+++ appnongsan/lib/widgets/login_textfield.dart
@@ -2,17 +2,19 @@ import 'package:flutter/material.dart';
 
 class LoginTextfield extends StatelessWidget {
   final String hintText;
+  final TextEditingController editingController;
   
-  LoginTextfield({super.key, required this.hintText});
+  LoginTextfield({super.key, required this.hintText, required this.editingController});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 350,
       height: 54,
-      child: TextField(
+      child: TextFormField(
+        controller: editingController,
         decoration:  InputDecoration(
-          // fillColor: Color.fromARGB(8, 194, 94, 1),
+          
             hintText: hintText,
             border: OutlineInputBorder(borderRadius: BorderRadius.circular(25))),
       ),
